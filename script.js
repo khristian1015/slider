098d9e3e61ef4178ba76a92d9061baf6029e0842
khristian1015 <khristian1015@hotmail.com>
2023-02-19 13:25:03 -0600

    separated into 3 files and changed slider into function

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 
 
-var slider1 = document.getElementById("myRange1");
+
+
+
+var slider = document.getElementById("rango1");
+var output = document.getElementById("valor1");
+
+sliderMove(slider, output);
+
+
+
+function sliderMove(slider, output) {
+    output.innerHTML = slider.value;
+    
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+      var sliderWidth = this.getBoundingClientRect().width;
+      console.log("sliderwidth", sliderWidth)
+      var outputWidth = output.getBoundingClientRect().width;
+      console.log("outputWidth", outputWidth)
+      var offset = this.value / (this.max - this.min) * sliderWidth - outputWidth / 2;
+      console.log("off", offset)
+      output.setAttribute('style', 'left: ' + offset + 'px');
+    }
+    
+    slider.oninput();
+}
+
+/* WORKS OK but chabged into function
+var slider1 = document.getElementById("myRange");
 var output1 = document.getElementById("valor1");
 output1.innerHTML = slider1.value;
 
@@ -16,4 +44,4 @@ slider1.oninput = function() {
 }
 
 slider1.oninput();
-
+*/
