36395a7ae6798b2ecf0699929af0f83048aaca45
khristian1015 <khristian1015@hotmail.com>
2023-02-23 20:02:33 -0600

    se agrega funcionalidad de recibir parametros en URL de encuesta y enviarlos

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 
-index.html
--- Archivo bueno de producción
--- slider cambia los valores conforme se mueve el slider, el valor cambiante sobre el slider (sigue la bolita)
+-index.html
+--- Archivo bueno de producción
+--- slider cambia los valores conforme se mueve el slider, el valor cambiante so
+bre el slider (sigue la bolita)
 
+index1.html
+
+index2.html
+-- slider básico como ejemplo (sin css)
+-- slider cambia los valores conforme se mueve el slider, el valor cambiante en un lugar fijo debajo del slider
+
+index-gpt.html
+codigo que me dio chatgpt donde me da el machote para varias preguntas pero con sliders básicos
+
+index3.html
+https://codepen.io/intertidalist/pen/VojWaR
+-- un slider cuya etiqueta se mueve exactamente como la bolita (no se desplaza izq ni derecha)
 
--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
 
+/* -Obtener parámetros-  */
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const correo = urlParams.get('mail');
+const trimestre = urlParams.get('trim');
+const proyecto = urlParams.get('proy');
+
+console.log("**********PARAMETROS");
+console.log(correo, trimestre, proyecto);
 
 
+/* Obtener info de sliders para que se mueva el VALOR  */
 const numPreguntas = 2;
 
 var slider1 = document.getElementById("rango1");
@@ -12,6 +22,7 @@ var slider2 = document.getElementById("rango2");
 var output2 = document.getElementById("valor2");
 sliderMove(slider2, output2);
 
+/* -Función para hacer que se mueva el VALOR de los sliders-  */
 function sliderMove(slider, output) {
   output.innerHTML = slider.value;
 
@@ -29,35 +40,41 @@ function sliderMove(slider, output) {
   slider.oninput();
 }
 
-
+/* Crear funcionalidad del botón: onClick  */
 const btn = document.getElementById('btnGuardar');
 btn.addEventListener('click', btnOnClick);
 
+/* Función para que el botón construya y mande info al backend */
 function btnOnClick() {
   const arrayCalifs = [slider1.value, slider2.value];
   console.log("califs", arrayCalifs)
 
+  /* Esto se elimina porque estban hardcodeados pruebas 
   const correo = "juan.rico@s4learning.com"
-  const trim = "1.ago22-oct22"
-  const proy = "comwhatsapp"
-  const calif = arrayCalifs
+  const trimestre = "1.ago22-oct22"
+  const proyecto = "comwhatsapp"*/
+  const calificaciones = arrayCalifs
 
   const URL = "http://localhost:5000/"
   const texto1 = "mail=" + correo
-  const texto2 = "trim=" + trim
-  const texto3 = "proy=" + proy
-  const texto4 = "cali=" + calif
-  textolargo = URL + "?" + texto1 + "&" + texto2 + "&" + texto3 + "&"
-    + texto4
+  const texto2 = "trim=" + trimestre
+  const texto3 = "proy=" + proyecto
+  const texto4 = "cali=" + calificaciones
+  textolargo = URL + "?" + texto1 + "&" + texto2 
+                + "&" + texto3 + "&" + texto4
   console.log(textolargo)
 
-
+  /* -Función para que el botón mande info al backend-  */
   fetch(textolargo)
     .then(response => response.json())
     .then(data => {
       // do something with the response
     });
-}
+  
+  /* -Función para que el botón mande llamar página de GRAX */
+  
+
+} // fin de la funcionalidad del botón
 
 
 /* WORKS OK but chabged into function
